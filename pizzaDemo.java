c4d80267ac7bec0e9b5a73ec82ac5ffe1c2d2955
Mohammad hussaini <mhussaini550@Mohammads-MacBook-Pro.local>
2022-12-20 22:50:31 -0500

    pizza Demo java file

---
+++ pizzaDemo.java
@@ -0,0 +1,23 @@
+package day42;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PizzaDemo {
+	public static void main(String[] args) {
+   Pizza dominos = new Pizza();
+   dominos.setPrice(15.99);
+   
+   List<String> mytoppings = new ArrayList<>();
+   mytoppings.add("cheese");
+   mytoppings.add("chicken");
+   mytoppings.add("viggie");
+   mytoppings.add("mushroom");
+  // mytoppings.add("onions"); // topping is not available
+   
+   dominos.setToppings(mytoppings);
+   System.out.println(dominos.getPrice());
+   System.out.println(dominos.getToppings());
+     	}
+	
+	}
